5054f5888ca65cb3a8bd077f3dbc904219c31cd2
xiemeiping <1035791926@qq.com>
2023-12-27 15:01:02 +0800

    add a link

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -74,3 +74,13 @@ language = 'en'
 locale_dirs = ['locale/'] 
 
 gettext_compact = False
+
+# open link in a new window
+
+rst_epilog = """
+
+.. |link_pc_monitor| raw:: html
+
+    <a href="https://www.sunfounder.com/blogs/news/raspberry-pi-system-monitor-for-pc-enhancing-performance-and-remote-monitoring" target="_blank">PC monitoring device</a>
+
+"""
