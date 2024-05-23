f196c0aef3fac98d327c90f895216da0f8797ce8
haru <email@example.com>
2024-05-23 13:43:30 +0800

    join facebook group

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -79,6 +79,10 @@ gettext_compact = False
 
 rst_epilog = """
 
+.. |link_sf_facebook| raw:: html
+
+    <a href="https://bit.ly/4aqLk44" target="_blank">here</a>
+
 .. |link_pc_monitor| raw:: html
 
     <a href="https://www.sunfounder.com/blogs/news/raspberry-pi-system-monitor-for-pc-enhancing-performance-and-remote-monitoring" target="_blank">PC monitoring device</a>
